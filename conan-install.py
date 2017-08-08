1bc0a89bf4a7b81fdbb2212f2f058f9909e8606f
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-08-08 22:16:59 +0200

    Abandoning conan-install.py (at least for now)

--- conan-install.py
+++ conan-install.py
@@ -1,5 +1,11 @@
 #!python3
 
+# DEPRECATED
+# This script was supposed to do "conan install" on nested project directories.
+# Since Conan does not work well with that pattern (at the time of writing: 2017-08-08),
+# I decided to scrap this approach, with the intent of beginning work on a script, or
+# collection of scripts, that handles more of the boilerplate work than just Conan.
+
 import os
 import sys
 import subprocess as sub
@@ -21,10 +27,15 @@ def install_directory(dir):
     sub.run(["conan"] + "install -g cmake_multi -s build_type=Debug   -s compiler.runtime=MDd --build=missing".split() + [dir])
     print("")
     
-for root, dirs, files in os.walk(root_dir):
-    print("(Examining directory \"%s\")" % root)
-    if any(f in files for f in ["conanfile.txt", "conanfile.py"]):
-        print("Found conanfile in directory %s, installing" % root)
-        install_directory(root)
-    dirs[:] = [d for d in dirs if d[0] != '.' and not d.startswith("build")]
-
+# DEPRECATED:
+# Crawling all the nest directories seemed like a good idea at the time, but Conan does not
+# cooperate well with nested project directories.
+if False:
+    for root, dirs, files in os.walk(root_dir):
+        print("(Examining directory \"%s\")" % root)
+        if any(f in files for f in ["conanfile.txt", "conanfile.py"]):
+            print("Found conanfile in directory %s, installing" % root)
+            install_directory(root)
+        dirs[:] = [d for d in dirs if d[0] != '.' and not d.startswith("build")]
+else:
+    install_directory(root_dir)
